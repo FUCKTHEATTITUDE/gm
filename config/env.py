393cb2d220f49e6a75ac71c53d79a5bb838bc4d4
ALAN WALKER <98202012+FUCKTHEATTITUDE@users.noreply.github.com>
2022-07-27 16:30:38 +0530

    Update env.py

--- config/env.py
+++ config/env.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = getenv('BOT_TOKEN')
-MONGO_URI = getenv('MONGO_URI')
-SUDO_USERS = list(map(int, getenv('SUDO_USERS').split()))
+BOT_TOKEN = getenv('BOT_TOKEN',"5458696791:AAHLcBi665AvhM9MA20YtqK64VzZ14a_U8E")
+MONGO_URI = getenv('MONGO_URI',"mongodb+srv://ALAN:20092001@cluster0.nmp83.mongodb.net/?retryWrites=true&w=majority")
+SUDO_USERS = list(map(int, getenv('SUDO_USERS',"1930954213").split()))
